f7c2dd32300c3cc8895b77df779501e86e5bf6d9
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2023-12-24 15:39:18 -0300

    Detalhes do filme

--- yoriflix/src/pages/Filme/index.js
+++ yoriflix/src/pages/Filme/index.js
@@ -1,7 +1,54 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from "../../services/api";
+
 function Filme() {
-    return(
-        <div>
-            <h1>Filme</h1>
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: "da1700a259636aa5359aaeb65d11a54b",
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=> {
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(()=> {
+                console.log("Erro ao carregar filme");
+            })
+        }
+
+        loadFilme();
+
+        return () => {
+            console.log('Componente foi desmontado')
+        }
+    }, [])
+
+    if(loading){
+        return(
+            <div className="filme-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
+    return (
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+            
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
         </div>
     )
 }
