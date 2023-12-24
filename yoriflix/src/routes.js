3e8642d6600b61892672ef2cfbb59c8fc4fe8ab1
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2023-12-24 15:17:42 -0300

    loading do filme

--- yoriflix/src/routes.js
+++ yoriflix/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Filme from './pages/Filme'
 
+import Erro from './pages/Erro'
+
 import Header from './components/Header'
 
 function RoutesApp() {
@@ -12,6 +14,9 @@ function RoutesApp() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/filme/:id" element={<Filme />} />
+
+
+                <Route path="*" element={<Erro />} />
             </Routes>
         </BrowserRouter>
     )
