e6b96396fae009ad5135cf8add883a1e54601d6d
Nícolas Machado Cardoso <nicolasxzx1225@gmail.com>
2023-12-28 09:19:23 -0300

    Atualizar o index.js

--- yoriflix/src/pages/Erro/index.js
+++ yoriflix/src/pages/Erro/index.js
@@ -5,7 +5,7 @@ function Erro() {
     return(
         <div className="not-found">
             <h1>404</h1>
-            <h2>Página não encontrada</h2>
+            <h2>Página não encontrada.</h2>
             <Link to="/">Ir para página inicial</Link>
         </div>
     )
